42a6748248de3643bb34d6fd5b7dacffafd768b2
Justin Lucero <justinluceroxd04@gmail.com>
2025-05-28 17:30:52 -0500

    Modificaciones

--- appangular/src/app/app.component.ts
+++ appangular/src/app/app.component.ts
@@ -1,13 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'appangular';
+  title = 'Angular WebSocket';
+  messages: string[] = [];
+  private socket: WebSocket | undefined;
+  constructor() {
+    this.connectWebSocket();
+  }
+  connectWebSocket() {
+    this.socket = new WebSocket('ws://localhost:3000'); 
+
+    this.socket.onopen = () => {
+      console.log('WebSocket connection established');
+    };
+
+    this.socket.onmessage = (event) => {
+      this.messages.push(event.data);
+    };
+
+    this.socket.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+
+    this.socket.onerror = (error) => {
+      console.error('WebSocket error:', error);
+    };
+  }
 }
