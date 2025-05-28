ff3211c29171bc9df3a4594fa86e70b5cba69b8c
Justin Lucero <justinluceroxd04@gmail.com>
2025-05-28 18:56:50 -0500

    Completado

--- appangular/src/app/app.component.ts
+++ appangular/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -13,24 +13,24 @@ export class AppComponent {
   title = 'Angular WebSocket';
   messages: string[] = [];
   private socket: WebSocket | undefined;
-  constructor() {
-    this.conectarWebSocket();
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.conectarWebSocket();
+    }
   }
+
   conectarWebSocket() {
     this.socket = new WebSocket('ws://localhost:3000');
-
     this.socket.onopen = () => {
       console.log('WebSocket conexion establecida');
     };
-
     this.socket.onmessage = (event) => {
       this.messages.push(event.data);
     };
-
     this.socket.onclose = () => {
       console.log('WebSocket conexion cerrada');
     };
-
     this.socket.onerror = (error) => {
       console.error('WebSocket error:', error);
     };
